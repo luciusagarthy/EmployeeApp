adf767d564650b0fbdebda451c87443ba6e35ce6
chromek.lukas@gmail.com <chromek.lukas@gmail.com>
2024-08-05 07:18:21 +0200

    05082024

--- Backend/Validators/UniqueEmailAttribute.cs
+++ Backend/Validators/UniqueEmailAttribute.cs
@@ -12,7 +12,6 @@ namespace Backend.Validators
 
             var _context = (EmployeeDbContext)context.GetService(typeof(EmployeeDbContext));
 
-            //_context.Employee.Any(e => e.Email == email && e.Id != id);
             if (_context.Employee.Any(e => e.Email == email))
             {
                 return new ValidationResult("Emailová adresa již v databázi existuje. Zadejte jinou emailovou adresu");
